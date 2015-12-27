45899e4f675c87cdbce0621185d267893f8fafd6
aTravelingN3rd <valentin.gerard.atn@gmail.com>
2015-12-27 19:51:02 +0200

    makeHTML.sh
    This file is used to create an html page in the script addUsers

---
+++ systemProgramming/scripts/makeHTML.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+#script creating index.html page
+
+echo "<HTML>"
+echo "<HEAD>"
+echo "  <TITLE>"
+echo "  Bienvenue sur votre espace personnel"
+echo "  </TITLE>"
+echo "</HEAD>"
+echo ""
+echo "<BODY>"
+echo "  Bonjour $1 $2"
+echo "</BODY>"
+echo "</HTML>"
